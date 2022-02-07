d1f7bdcb444ea1908645b9ddac04c0ae747a5039
Maksim_Shalavin <shalavinms@gmail.com>
2022-02-07 16:02:52 +0300

    binary tree

---
+++ datastruct/tree/binary.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type BinaryTree struct {
+	Root *Leaf
+}
+
+type Leaf struct {
+	Left  *Leaf
+	Right *Leaf
+	Value int
+}
+
+func New() *BinaryTree {
+	return &BinaryTree{
+		Root: nil,
+	}
+}
+
+func (b *BinaryTree) Delete(val int) {}
+
+func (b *BinaryTree) Add(val int) {
+	b.Root = add(b.Root, val)
+}
+
+func add(leaf *Leaf, val int) *Leaf {
+	if leaf == nil {
+		return &Leaf{
+			Left:  nil,
+			Right: nil,
+			Value: val,
+		}
+	}
+	switch {
+	case leaf.Value > val:
+		leaf.Left = add(leaf.Left, val)
+	case leaf.Value < val:
+		leaf.Right = add(leaf.Right, val)
+	case leaf.Value == val:
+		return leaf
+	}
+	return leaf
+}
+
+func (b *BinaryTree) Search(val int) {}
+
+func main() {
+	myTree := New()
+	myTree.Add(9)
+	myTree.Add(5)
+	myTree.Add(13)
+	myTree.Add(4)
+	myTree.Add(8)
+	fmt.Println(myTree.Root)
+}
