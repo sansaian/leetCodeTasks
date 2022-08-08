aeebd0de2b6173008d2ee4205d4f7fdd39d70d3a
saian <shalavinms@gmail.com>
2022-08-08 16:22:38 +0300

    merge sorted arr and binary tree

---
+++ sorting_serching/first_bad_version/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println(Search([]int{1, 2, 3, 4, 5}, 4))
+	//fmt.Println(Search([]int{1, 2, 3, 4, 5}, 1))
+	fmt.Println(Search([]int{1, 2}, 1))
+}
+
+func Search(arr []int, value int) int {
+	return searchBinary(arr, value, 0, len(arr)-1)
+}
+
+func searchBinary(arr []int, value int, low int, high int) int {
+	if low >= high {
+		return -1
+	}
+	mid := low + (high-low)/2
+
+	if arr[mid] == value {
+		return mid
+	}
+	if arr[mid] < value {
+		return searchBinary(arr, value, mid+1, high)
+	}
+	if arr[mid] > value {
+		return searchBinary(arr, value, low, mid-1)
+	}
+	return -1
+}
---
+++ sorting_serching/main.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func merge(nums1 []int, nums2 []int) []int {
+	result := make([]int, 0, len(nums1)+len(nums2))
+	var j, k int
+
+	for i := 0; i <= len(nums1)+len(nums2); i++ {
+		if j < len(nums1) && k < len(nums2) {
+			if nums1[j] <= nums2[k] {
+				result = append(result, nums1[j])
+				j++
+				continue
+			} else {
+				result = append(result, nums2[k])
+				k++
+				continue
+			}
+		}
+		if j < len(nums1) {
+			result = append(result, nums1[j])
+			j++
+			continue
+		} else if k < len(nums2) {
+			result = append(result, nums2[k])
+			k++
+		}
+
+	}
+	return result
+}
+
+func mergeLeetCode(nums1 []int, m int, nums2 []int, n int) {
+
+	copyNyms1 := make([]int, m)
+	copy(copyNyms1, nums1[:m])
+	var j, k int
+
+	for i := 0; i < m+n; i++ {
+		if j < m && k < n {
+			if copyNyms1[j] <= nums2[k] {
+				nums1[i] = copyNyms1[j]
+				j++
+				continue
+			} else {
+				nums1[i] = nums2[k]
+				k++
+				continue
+			}
+		}
+		if j < m {
+			nums1[i] = copyNyms1[j]
+			j++
+		}
+		if k < n {
+			nums1[i] = nums2[k]
+			k++
+		}
+	}
+	fmt.Println(nums1)
+}
+
+func main() {
+	fmt.Println(merge([]int{3, 5, 8, 9}, []int{2, 5, 6, 10}))
+	fmt.Println(merge([]int{1, 2, 3, 6, 8}, []int{2, 5, 6}))
+
+	mergeLeetCode([]int{3, 5, 8, 9, 0, 0, 0, 0}, 4, []int{2, 5, 6, 10}, 4)
+	mergeLeetCode([]int{1, 2, 3, 6, 8, 0, 0, 0}, 4, []int{2, 5, 6}, 3)
+
+}
