e14b000c1da543b221bfbfc1266b5eb6e55396d9
saian <shalavinms@gmail.com>
2022-09-12 11:47:32 +0300

    delete node

--- linkedlist/delete_node/main.go
+++ linkedlist/delete_node/main.go
@@ -1,24 +1,37 @@
 package main
 
-type List struct {
-	root ListNode // sentinel list element, only &root, root.prev, and root.next are used
-	len  int      // current list length excluding (this) sentinel element
-}
-
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
-
-func deleteNode(node *ListNode) {
-	if node.Next == nil {
-		node = nil
-		return
-	}
-	node.Val = node.Next.Val
-	node.Next = node.Next.Next
-}
+import "github.com/leetCodeTasks/datastruct/list"
 
 func main() {
+	//exampleList := list.New([]int{1, 2, 6, 3, 4, 6, 6})
+	//exampleList := list.New([]int{7, 7, 7, 7})
+	exampleList := list.New([]int{5, 4, 3, 2, 1, 1})
 
+	exampleList.Print()
+	removeElements(exampleList, 1)
+	exampleList.Print()
+}
+
+func removeElements(head *list.ListNode, val int) *list.ListNode {
+	remove(head, val)
+	if head != nil && head.Val == val {
+		head = nil
+	}
+	return head
+}
+
+func remove(head *list.ListNode, val int) {
+	switch {
+	case head == nil || head.Next == nil:
+		return
+	case head.Val == val:
+		head.Val = head.Next.Val
+		head.Next = head.Next.Next
+		remove(head, val)
+		return
+	case head.Next.Val == val:
+		head.Next = head.Next.Next
+		remove(head, val)
+	}
+	remove(head.Next, val)
 }
