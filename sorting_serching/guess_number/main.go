ea2e3853e91179122325d3f3e8a019ebcee90b15
sansaian <shalavinms@gmail.com>
2022-11-16 13:30:41 +0400

    several tasks

---
+++ sorting_serching/guess_number/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(guessNumber(1))
+}
+
+func guessNumber(n int) int {
+	return binarySearch(0, n)
+}
+
+func binarySearch(start, finish int) int {
+	if start > finish {
+		return -1
+	}
+	dot := (finish-start)/2 + start
+
+	switch guess(dot) {
+	case -1:
+		return binarySearch(start, dot-1)
+	case 1:
+		return binarySearch(dot+1, finish)
+	default:
+		return dot
+	}
+}
+
+func guess(dot int) int {
+	goal := 1
+	if dot < goal {
+		return 1
+	}
+	if dot > goal {
+		return -1
+	}
+	return 0
+}
