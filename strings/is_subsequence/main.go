80c4abce306c931c059208dde2067ef9313e6ba4
saian <shalavinms@gmail.com>
2024-01-15 17:54:20 +0100

    find largest value

---
+++ strings/is_subsequence/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(isSubsequence("abc", "ahbgdc"))
+	fmt.Println(isSubsequence("axc", "ahbgdc"))
+}
+
+func isSubsequence(s string, t string) bool {
+	var i, j int
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+	return i == len(s)
+}
